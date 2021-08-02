20c11d8f64f9e5415b17c81ce544a41504c6225f
kiwoon Kim <87232579+tigerenergy@users.noreply.github.com>
2021-08-02 22:04:00 +0900

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,15 +1,135 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
 
+
+
+/**
+ * Textures
+ */
+ const textureLoader = new THREE.TextureLoader()
+ const cubeTextureLoader = new THREE.CubeTextureLoader()
+
+
+ const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
+ const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+ const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+ const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
+ const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
+ const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+ const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+ const matcapTexture = textureLoader.load('/textures/matcaps/1.png')
+ const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
+ gradientTexture.minFilter = THREE.NearestFilter
+ gradientTexture.magFilter = THREE.NearestFilter
+ gradientTexture.generateMipmaps = false
+
+ const environmentMapTexture = cubeTextureLoader.load([
+    '/textures/environmentMaps/0/px.jpg',
+    '/textures/environmentMaps/0/nx.jpg',
+    '/textures/environmentMaps/0/py.jpg',
+    '/textures/environmentMaps/0/ny.jpg',
+    '/textures/environmentMaps/0/pz.jpg',
+    '/textures/environmentMaps/0/nz.jpg'
+])
 /**
  * Base
  */
+
+
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+
+/**
+ * Objects
+ */
+//  const material = new THREE.MeshBasicMaterial()
+    // material.color = new THREE.Color(0xff0000) 
+    // material.wireframe = true
+    // material.opacity = 0.5
+    // material.transparent = true
+    // material.alphaMap = doorAlphaTexture
+    // material.side = THREE.DoubleSide
+
+//  const material = new THREE.MeshNormalMaterial()
+//  material.flatShading = true
+
+
+//  const material = new THREE.MeshMatcapMaterial()
+//  material.matcap = matcapTexture
+
+// const material = new THREE.MeshDepthMaterial()
+// const material = new THREE.MeshPhongMaterial()
+// const material = new THREE.MeshToonMaterial()
+// const material = new THREE.MeshStandardMaterial()
+// material.metalness = 0
+// material.roughness = 1
+// material.gradientMap = gradientTexture
+// material.shininess = 100
+// material.specular = new THREE.Color(0x1188ff)
+// material.map = doorColorTexture
+// material.normalMap = doorNormalTexture
+// material.normalScale.set(0.5, 0.5)
+// material.transparent = true
+// material.alphaMap = doorAlphaTexture
+
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 0.7
+material.roughness = 0.2
+
+ const sphere = new THREE.Mesh(
+     new THREE.SphereGeometry(0.5, 64, 64),
+     material
+ )
+ sphere.position.x = - 1.5
+ sphere.geometry.setAttribute('uv2', new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2))
+ const plane = new THREE.Mesh(
+     new THREE.PlaneGeometry(1, 1, 100, 100),
+     material
+ )
+  plane.geometry.setAttribute('uv2', new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2))
+ 
+ const torus = new THREE.Mesh(
+     new THREE.TorusGeometry(0.3, 0.2, 64, 128),
+     material
+ )
+ torus.geometry.setAttribute('uv2', new THREE.BufferAttribute(torus.geometry.attributes.uv.array, 2))
+ torus.position.x = 1.5
+ material.aoMap = doorAmbientOcclusionTexture
+ material.aoMapIntensity = 1
+ material.displacementMap = doorHeightTexture
+ material.displacementScale = 0.05
+//  material.metalnessMap = doorMetalnessTexture
+//  material.roughnessMap = doorRoughnessTexture
+ material.envMap = environmentMapTexture
+ scene.add(sphere, plane, torus)
+
+/**
+ * Debug
+ */
+ const gui = new dat.GUI()
+
+gui.add(material, 'aoMapIntensity').min(0).max(1).step(0.0001)
+gui.add(material, 'displacementScale').min(0).max(1).step(0.0001)
+gui.add(material, 'metalness').min(0).max(1).step(0.0001)
+gui.add(material, 'roughness').min(0).max(1).step(0.0001)
+/**
+ * Lights
+ */
+ const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+ scene.add(ambientLight)
+
+ const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
+
 
 /**
  * Sizes
@@ -65,6 +185,16 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+
+    sphere.rotation.y = 0.1 * elapsedTime
+    plane.rotation.y = 0.1 * elapsedTime
+    torus.rotation.y = 0.1 * elapsedTime
+
+    sphere.rotation.x = 0.15 * elapsedTime
+    plane.rotation.x = 0.15 * elapsedTime
+    torus.rotation.x = 0.15 * elapsedTime
 
     // Update controls
     controls.update()
